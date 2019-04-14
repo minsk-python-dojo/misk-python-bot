d6bfc66570972da1f44375f4ef1df2597302d8f9
hartred <artiomse@gmail.com>
2019-04-14 14:45:38 +0300

    rewritten init, added generic method to call Bot API commands, removed bot token setter

--- core/bot.py
+++ core/bot.py
@@ -1,8 +1,19 @@
+import logging
 import typing
 
 import requests
 
-from core.exceptions import (ConnectionError, APIConnectionError, IdentityError)
+from core.api_commands import ALL_COMMANDS
+from core.exceptions import (
+    HTTPConnectionError,
+    APIConnectionError,
+    IdentityError,
+    InvalidCommand,
+)
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class Bot:
@@ -10,30 +21,39 @@ class Bot:
     __url: str = 'https://api.telegram.org/bot'
     
     def __init__(self, token: str) -> None:
-        self.__token: typing.Union[str, None] = None
-        self.token = token
+        self.__token: typing.Union[str, None] = token
+        self.__check_token()
 
     @property
     def token(self) -> typing.Union[str, None]:
         return self.__token
 
-    @token.setter
-    def token(self, value: str) -> None:
-        response = self.get_me(value)
-        if response.status_code != requests.codes.ok:
-            raise ConnectionError
+    def __check_token(self) -> None:
+        response = self.get_me()
+        ret_status = response.status_code
+        if ret_status != requests.codes.ok:
+            logger.info(f'Got unexpected status code {ret_status}')
+            raise HTTPConnectionError
         response_json: dict = response.json()
         if not response_json.get('ok', False):
             raise APIConnectionError
         is_bot: bool = response_json.get('result', {}).get('is_bot', False)
         if not is_bot:
             raise IdentityError
-        self.__token: typing.Union[str, None] = value
-            
-        
 
-    def get_me(self, value: str) -> requests.Response:
-        return requests.get(f'{self.__url}{value}/getMe')
+    def get_me(self) -> requests.Response:
+        return self.invoke_command(ALL_COMMANDS.GET_ME)
+
+    def get_update(self):
+        return self.invoke_command(ALL_COMMANDS.GET_UPDATES)
+
+    def invoke_command(self, command: str) -> requests.Response:
+        if command not in ALL_COMMANDS:
+            raise InvalidCommand(f'Try execute invalid command: {command}')
+
+        command_endpoint = f'{self.__url}{self.__token}/{command}'
+        logger.info(f'{command_endpoint}')
+        return requests.get(command_endpoint)
 
     
 
